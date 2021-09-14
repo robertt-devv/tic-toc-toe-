b95b7bbf185240a09d216505e19b6c7de89b6b59
roberto-deev <roberto.deev@gmail.com>
2021-09-13 23:43:19 -0300

    motor/nucleo do jogo

---
+++ _jsc/core.js
@@ -0,0 +1,116 @@
+class Gcore {
+	game_boardA = new Array(9).fill(0)
+	
+	constructor(){
+		this.last_player = '1'; // 1:X,  2:O
+		this.user_symbol = 'X' 
+		this.game_boardA = new Array(9).fill(0)
+	}
+	
+	_$(objName) {
+		return document.querySelectorAll(objName)
+	}
+	
+	arrayGroup(src,groups) {
+		let array = new Array() 
+		let _src = src.slice();
+		let pairs = src.length/groups
+		
+		while (pairs > 0) {
+			array.push(_src.splice(0,groups).join('')); pairs--;
+		}
+		return array
+	}
+	
+	cleanBoard(objName) {
+		let obj = this._$(objName);
+		
+		obj.forEach((va,id,ve)=>{
+			ve[id].innerText = '\n'; 
+			this.game_boardA[id] = 0;
+		});
+		return 0;
+	}
+	
+	
+	markHome(homeId,player,position) {
+ 		let _player = ( player == 'X' ) ? 1 : 2;
+ 	    let nameObj = this._$(homeId)[position];
+ 	    
+ 	    if (this.game_boardA[position] == 0 ){
+ 	    	this.game_boardA[position] = _player;
+ 	    	nameObj.innerText = (_player == 1) ? 'X' : 'O';
+ 	    	return 0;
+ 	    }
+ 	    
+ 	    return 1;
+	}
+	
+	fakeIACode(){
+	    let pBoard = this.arrayGroup(this.game_boardA,3);
+	    
+	}
+	
+	 cpuPlay() {
+	     let pBoard = this.game_boardA; /* pBoard (p, "poteiro") */
+	     let $1victory = this.haveWinner('X','y');
+	     let $2victory = this.haveWinner('O','y');
+	     try{
+	        for (let lp=0; lp<3; lp++){
+	             if (Number(pBoard[$2victory.charAt(lp)]) == 0 ){return Number($2victory.charAt(lp));}
+	             if (Number(pBoard[$1victory.charAt(lp)]) == 0 ){return Number($1victory.charAt(lp));}
+	        }
+	     }catch(errr){
+	         setTimeout(()=>{this.fakeIACode()},2000)
+	         //return this.fakeIACode()
+	         console.log('erro, haveWine retornou NaN')
+	     }
+	}
+	
+	haveWinner(player,prewin) {
+		let pBoard = this.game_boardA;
+		
+		let playID = ( player == 'X' ) ? 1 : 2;
+		let preWin = ( prewin == 'y' ) ? 1 : 0; /* pre vitoria */
+		
+		const  victS = [ '012','345','678','036',
+                         '147','258','048','246' ] // indices de vitoria
+                         
+        let counter = 0; /* conta quantos, (1 || 2) ,  (X || O)  */
+        let chJoker = 0; /* Caracter coringa (Zero), ver linha 7 */
+        
+        for (let x=0; x<8; x++){//for1
+        for (let y=0; y<3; y++){//for2  
+        /*board [R,1,2,3,4..] victS[0][1] = 0, board[victS] = R */
+            let  gbIdVal = Number(pBoard[victS[x][y]])
+            counter += (gbIdVal == playID ) ? 1 : 0; 
+            chJoker += (gbIdVal == 0 ) ? 1 : 0;
+        }//for2
+            if (preWin == 1 && chJoker == 1 ){ return victS[x];} /* board -> 110 */
+            if (preWin == 0 && counter == 3 ){ return victS[x];} /* board -> 111 */
+            
+            counter = 0; chJoker = 0;
+        }//for1
+                        
+	    return NaN;
+	}
+	
+}
+/*
+let gameCore = new Gcore();
+
+window.onload = function(){
+	
+	let buttons = gameCore._$('.-box');
+	let player = 'X';
+	
+	for (let lp=0; lp<9; lp++){
+		buttons[lp].addEventListener('click',()=>{
+			gameCore.markHome('.-box',player,lp);
+			player = (player == 'X') ? 'O' : 'X';
+			console.log(gameCore.haveWinner('X'));
+		})
+	}
+	
+}
+*/
---
+++ _jsc/main.js
@@ -0,0 +1,71 @@
+var victoryTexts = new Array();
+    victoryTexts.push('Muito bem, so nao e melhor doque\'eu');
+    victoryTexts.push('Roubando assim!, todo mundo ganha ne!?');
+    victoryTexts.push('Foi sorte, vamos outra partida');
+    victoryTexts.push('Nao acredito que perdi para vc! ');
+    victoryTexts.push('Nao sei oque vc ta fazendo, mas ta roubando de alguma forma');
+    victoryTexts.push('Idiota, filha da cunha. Vamos jogar serio');
+    victoryTexts.push('Ja chega, nao jogo com ladrao, desisto');
+    victoryTexts.push('Meu Deus!, como vc esta fazendo isso');
+    victoryTexts.push('Se eu tivesse pernas e braços, de enchia de porrada');
+    
+    
+
+window.onload = function() {
+    anim.borderDraw('.lines'); // animation X and Y lines 
+    anim.pressHome('.-box'); // animation press home
+}
+
+var core = new Gcore();
+var anim = new Ganim();
+    
+var borderButtons = core._$('.-box');
+var turnPlayer = 'X';
+var typeGame = 1;
+var iCanPlay = true;
+var playerOPoints = 0;
+var playerXPoints = 0;
+
+
+document.addEventListener('click',()=>{
+    let playerX = core.haveWinner('X','n');
+    let playerO = core.haveWinner('O','n');
+ 
+    if (!isNaN(playerO) && iCanPlay == true){
+       anim.vitory('.-box',playerO); iCanPlay=false
+        playerOPoints +=1;
+        core._$('#pts_1')[0].innerText = `${playerOPoints} Pontos`;
+    } 
+            
+    if (!isNaN(playerX) && iCanPlay == true){
+        anim.vitory('.-box',playerX); iCanPlay=false
+        playerXPoints += 1;
+        core._$('#pts_2')[0].innerText = `${playerXPoints} Pontos`;
+        anim.writeText(victoryTexts[Math.round(Math.random()*8)],'#msgText',50)
+    }
+    
+});
+    
+    
+function mark_home(lp){
+    if ( iCanPlay != true ) return 1;
+    
+    if ( typeGame == 2 ){
+        if (core.markHome('.-box',turnPlayer,lp) == 0 ){
+            anim.playSound('#markHome');
+            turnPlayer = (turnPlayer == 'X') ? 'O' : 'X';
+        }
+    }else{
+        if (turnPlayer === 'X'){
+            let status = core.markHome('.-box','X',lp);
+            if (status == 0) turnPlayer = 'O';
+        }
+        core.markHome('.-box','O',core.cpuPlay());
+        turnPlayer = 'X';
+    }
+}
+    
+function clean_board(){
+    core.cleanBoard('.-box');
+    iCanPlay = true;
+}
